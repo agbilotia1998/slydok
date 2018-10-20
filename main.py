4acae9792618f331dcfe9dacd4b1340544d4cf64
WickedBrat <harshsrivastav123@gmail.com>
2018-10-20 06:45:13 +0530

    Added document heading extraction

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 from io import BytesIO, FileIO
+from docx import Document
+
+document = Document('test.docx')
+
+def iter_headings(paragraphs):
+    for paragraph in paragraphs:
+        if paragraph.style.name.startswith('Heading'):
+            yield paragraph
+
+for para in iter_headings(document.paragraphs):
+    print(para.text)
 
 prs = Presentation()
 title_slide_layout = prs.slide_layouts[0]

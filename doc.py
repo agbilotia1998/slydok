f5cbcde4dea4e06421e0037a27b8c6bd993df9dc
Harshit Jain <hj.harshit007@gmail.com>
2018-10-20 10:43:11 +0530

    Added doc parse by font sizes

--- doc.py
+++ doc.py
@@ -1,25 +1,39 @@
 from docx import Document
 import pprint
+from collections import OrderedDict
 
 pp = pprint.PrettyPrinter(indent=4)
 
 document = Document('test.docx')
 
-store = {}
+def convert(p):
+    return p.text
 
-def analyze(paragraphs, level):
-    heading = 'Heading {0}'.format(level)
+def find_all_font_sizes(paragraphs):
+    sizes = set()
+
+    for p in paragraphs:
+        font_size = p.style.font.size
+        sizes.add(font_size)
+
+    return sorted(list(sizes), reverse=True)
+
+def analyze_by_size(paragraphs, sizes):
+    if (sizes[0] == None):
+        return map(convert, paragraphs)
+
+    cur_size = sizes[0]
     lines = []
     headings = 0
     prev_heading = ''
-    d = {}
+    d = OrderedDict()
 
     for p in paragraphs:
-        if p.style.name == heading:
+        if p.style.font.size == cur_size:
             if headings > 0:
-                d[prev_heading] = analyze(lines, level + 1)
+                d[prev_heading] = analyze_by_size(lines, sizes[1:])
             elif headings == 0 and len(lines) > 0:
-                d['intro_text'] = map(lambda x: x.text, lines)
+                d['intro_text'] = map(convert, lines)
 
             prev_heading = p.text
             d[prev_heading] = {}
@@ -29,11 +43,42 @@ def analyze(paragraphs, level):
             lines.append(p)
 
     if headings > 0:
-        d[prev_heading] = analyze(lines, level + 1)
+        d[prev_heading] = analyze_by_size(lines, sizes[1:])
 
         return d
     else:
-        return map(lambda x: x.text, lines)
+        return map(convert, lines)
 
-store = analyze(document.paragraphs, 1)
+def analyze_by_heading(paragraphs, level):
+    heading = 'Heading {0}'.format(level)
+    lines = []
+    headings = 0
+    prev_heading = ''
+    d = OrderedDict()
+
+    for p in paragraphs:
+        if p.style.name == heading:
+            if headings > 0:
+                d[prev_heading] = analyze(lines, level + 1)
+            elif headings == 0 and len(lines) > 0:
+                d['intro_text'] = map(convert, lines)
+
+            prev_heading = p.text
+            d[prev_heading] = {}
+            headings += 1
+            lines = []
+        else:
+            lines.append(p)
+
+    if headings > 0:
+        d[prev_heading] = analyze_by_heading(lines, level + 1)
+
+        return d
+    else:
+        return map(convert, lines)
+
+# store = analyze(document.paragraphs, 1)
+# pp.pprint(store)
+l = find_all_font_sizes(document.paragraphs)
+store = analyze_by_size(document.paragraphs, l)
 pp.pprint(store)
